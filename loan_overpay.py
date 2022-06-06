cdc05be7ccb86177644390574237501ad356647b
Jakub Peczke <jpeczke@opera.com>
2022-06-06 14:46:50 +0200

    Fix case when capital is already payed, but installments still executed

--- loan_overpay.py
+++ loan_overpay.py
@@ -9,15 +9,18 @@ def calculate_loan_installment(
     loan_value_pln: int, lease_percentage: float, number_of_installments: int, overpay_pln: int = 0,
 ) -> float:
     loan_value_gr = loan_value_pln * 100
-    loan_capital_left = loan_value_gr - (overpay_pln * 100)
+    loan_capital_left_gr = loan_value_gr - (overpay_pln * 100)
     already_payed_gr = 0
     initial_installment_value = int(loan_value_gr / number_of_installments)
 
     for interation in range(number_of_installments):
         already_payed_gr += _calculate_single_installment(
-            lease_percentage, loan_capital_left, initial_installment_value
+            lease_percentage, loan_capital_left_gr, initial_installment_value
         )
-        loan_capital_left -= initial_installment_value
+        loan_capital_left_gr -= initial_installment_value
+        if loan_capital_left_gr <= 0:
+            already_payed_gr += loan_capital_left_gr
+            break
 
     total_payed_pln = (already_payed_gr / 100) + overpay_pln
 
@@ -30,5 +33,7 @@ def calculate_loan_installment(
 
 
 # Use it like this:
-calculate_loan_installment(300000, 7.50, 300)
-calculate_loan_installment(300000, 7.50, 300, overpay_pln=20000)
+calculate_loan_installment(97366, 4.75, 254, overpay_pln=97366)
+calculate_loan_installment(97366, 4.75, 254)
+calculate_loan_installment(97366, 4.75, 254, overpay_pln=40000)
+
